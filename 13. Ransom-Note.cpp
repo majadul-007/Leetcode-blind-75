a41cb36f59a5d869006559942d2181aae34643b6
Tusar Haque <tusarhaque12@gmail.com>
2023-03-03 18:30:11 +0600

    first

---
+++ 13. Ransom-Note.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+	bool canConstruct(string ransomNote, string magazine) {
+
+		int count[26] = {0};
+
+		if (ransomNote.size() > magazine.size()) {
+
+			return false;
+		}
+
+		for (char c : magazine) {
+
+			count[c - 'a']++;
+		}
+
+		for (char c : ransomNote) {
+
+			if (count[c - 'a']-- == 0) {
+				return false;
+			}
+		}
+		return true;
+
+
+	}
+};
