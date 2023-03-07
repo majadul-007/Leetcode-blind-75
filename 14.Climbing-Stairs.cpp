fc383faefdda1e967a13f748f816540a222491c6
Tusar Haque <tusarhaque12@gmail.com>
2023-03-07 11:48:45 +0600

    first

---
+++ 14.Climbing-Stairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        if(n==1){
+            return 1;
+            }
+            
+    //create vector dp with n+1 size
+
+    vector<int> dp(n+1);
+
+    //set 0, 1, 2th value for dp
+
+    dp[0]=0; dp[1]=1; dp[2]=2;
+
+    for(int i=3; i<=n; i++){
+
+        //store the sum value of previous value of n and values before that
+
+        dp[i] = dp[i-1]+ dp[i-2];
+
+    }
+
+    return dp[n];
+        
+    }
+};
