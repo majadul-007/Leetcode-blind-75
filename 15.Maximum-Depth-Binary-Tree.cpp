c147b131e5512a6e181b9fd7f811de49d825bc9e
Tusar Haque <tusarhaque12@gmail.com>
2023-03-08 11:58:53 +0600

    first

---
+++ 15.Maximum-Depth-Binary-Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+
+        //check if root node is null or not
+
+        if(root==NULL){
+            return 0;
+        }
+
+        //traverse the left and right subtree
+
+        int lt = maxDepth(root->left);
+        int rt = maxDepth(root->right);
+
+        //return the max value of from root  node
+
+        return max(lt, rt) + 1;
+        
+    }
+};
